0b4652b1b728d859b6e6d3e03933dcd4b4c5f8bd
root <root@LAHAV-PC.localdomain>
2026-06-28 20:11:00 +0300

    Initial payments service

--- test.js
+++
@@ -1,6 +0,0 @@
-// Smoke tests for the payments service
-const assert = require('assert');
-console.log('Running payments service smoke tests...');
-assert.strictEqual(typeof require('./index.js'), 'object' || 'function', true);
-assert.ok('PAY-'.startsWith('PAY-'));
-console.log('All tests passed.');
